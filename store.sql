967977f024852df0d16524e6d0e1de181bfc7ac1
Matt Caruso <40008209+Matt-Caruso@users.noreply.github.com>
2022-04-18 14:18:41 -0400

    hw6

--- store.sql
+++ store.sql
@@ -19,15 +19,39 @@ CREATE TABLE orders
     FOREIGN KEY (customerId) REFERENCES customers (id)
 );
 
+CREATE TABLE customers_order_association
+(
+    id         int  AUTO_INCREMENT
+    primary key,
+    customers_id int not null,
+    orders_id int not null,
+    constraint id
+    unique (id)
+);
+
 INSERT INTO customers (firstname, lastname, company) VALUES ( 'Matt', 'Ryan', 'Microsoft');
 INSERT INTO customers (firstname, lastname, company) VALUES ( 'Joseph', 'Neal', 'Google');
+INSERT INTO customers (firstname, lastname, company) VALUES ( 'greg', 'bonny', 'Meta');
 
 
 INSERT INTO orders (product, cost, customerId) VALUES ('Laptop', '1000', 1);
-INSERT INTO orders (product, cost, customerId) VALUES ('phone', '500', 1);
+INSERT INTO orders (product, cost, customerId) VALUES ('phone', '500', 2);
+INSERT INTO orders (product, cost, customerId) VALUES ('Desktop', '1500', 3);
 
-INSERT INTO orders (product, cost, customerId) VALUES ('Laptop', '2000', 2);
-INSERT INTO orders (product, cost, customerId) VALUES ('phone', '400', 2);
+
+INSERT INTO customers_order_association (customers_id, orders_id) VALUES (1,1);
+INSERT INTO customers_order_association (customers_id, orders_id) VALUES (2,2);
+INSERT INTO customers_order_association (customers_id, orders_id) VALUES (2,2);
 
 SELECT * from orders;
 SELECT * from customers;
+SELECT * from customers_order_association;
+
+SELECT * from orders
+Limit 1;
+
+SELECT * from customers
+LIMIT 2;
+
+SELECT * from customers_order_association
+LIMIT 3;
