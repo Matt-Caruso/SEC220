3db28b72296f8a6f8842214977f6954456a8cca1
Matt Caruso <40008209+Matt-Caruso@users.noreply.github.com>
2022-05-10 22:35:44 -0400

    Update hw7Updated.sql

--- hw7Updated.sql
+++ hw7Updated.sql
@@ -35,13 +35,57 @@ create table orders
 create index customerId
     on orders (customerId);
     
-create table warehouse
-(
-    id       int auto_increment
-        primary key,
-    location varchar(20) null,
-    constraint id
-        unique (id)
-);
+INSERT INTO customers (firstname, lastname, company) VALUES ( 'Matt', 'Ryan', 'Microsoft');
+INSERT INTO customers (firstname, lastname, company) VALUES ( 'Joseph', 'Neal', 'Google');
+INSERT INTO customers (firstname, lastname, company) VALUES ( 'greg', 'bonny', 'Meta');
+INSERT INTO customers (firstname, lastname, company) VALUES ( 'jett', 'bonny', 'Apple');
+INSERT INTO customers (firstname, lastname, company) VALUES ( 'Mike', 'fisher', 'logitech');
+
+
+
+
+INSERT INTO orders (product, cost, customerId) VALUES ('Laptop', '1000', 1);
+INSERT INTO orders (product, cost, customerId) VALUES ('phone', '500', 2);
+INSERT INTO orders (product, cost, customerId) VALUES ('Desktop', '1500', 3);
+INSERT INTO orders (product, cost, customerId) VALUES ('Desktop', '1200', 2);
+INSERT INTO orders (product, cost, customerId) VALUES ('phone', '500', 4);
+INSERT INTO orders (product, cost, customerId) VALUES ('xbox', '299', 5);
+
+INSERT INTO customers_order_association (customers_id, orders_id) VALUES (1,1);
+INSERT INTO customers_order_association (customers_id, orders_id) VALUES (2,2);
+INSERT INTO customers_order_association (customers_id, orders_id) VALUES (3,3);
+INSERT INTO customers_order_association (customers_id, orders_id) VALUES (2,2);
+INSERT INTO customers_order_association (customers_id, orders_id) VALUES (4,4);
+INSERT INTO customers_order_association (customers_id, orders_id) VALUES (5,5);
+
+
+SELECT * from orders;
+SELECT * from customers;
+SELECT * from customers_order_association;
+
+SELECT * from orders
+Limit 1;
+
+SELECT * from customers
+LIMIT 2;
+
+SELECT * from customers_order_association
+LIMIT 3;
+
+DELETE FROM customers WHERE FirstName='Matt';
+DELETE FROM customers WHERE FirstName='Joseph';
+
+
+SELECT Orders.customerId, Customers.firstname
+FROM Orders
+INNER JOIN Customers ON Orders.CustomerID = Customers.id;
+
+SELECT Orders.customerId, Customers.firstname
+FROM Orders
+INNER JOIN Customers ON Orders.CustomerID = Customers.id where customerId = 2;
+
+SELECT Orders.customerId, Customers.firstname
+FROM Orders
+INNER JOIN Customers ON Orders.CustomerID = Customers.id where customerId = 3;
 
 
